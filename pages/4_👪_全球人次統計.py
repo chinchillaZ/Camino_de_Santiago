2f84c37eb6678463af74125c37a796e562fe32a7
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-20 02:24:29 +0800

    Update 4_👪_全球人次統計.py

--- pages/4_👪_全球人次統計.py
+++ pages/4_👪_全球人次統計.py
@@ -39,4 +39,41 @@ if lower_row[2].button("英國之路", use_container_width=True):
 if lower_row[3].button("世界盡頭之路", use_container_width=True):
     lower_row[3].markdown("You clicked 世界盡頭之路")
 
-#m.to_streamlit(height=700)
+
+chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+
+st.pydeck_chart(
+    pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=chart_data,
+                get_position="[lon, lat]",
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+            pdk.Layer(
+                "ScatterplotLayer",
+                data=chart_data,
+                get_position="[lon, lat]",
+                get_color="[200, 30, 0, 160]",
+                get_radius=200,
+            ),
+        ],
+    )
+)
+
+m.to_streamlit(height=700)
