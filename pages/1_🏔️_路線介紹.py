eefbdfcc72988f8c0c8c382bbdeb729dce2bab6c
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-19 12:24:53 +0800

    Update 1_🏔️_路線介紹.py

--- pages/1_🏔️_路線介紹.py
+++ pages/1_🏔️_路線介紹.py
@@ -31,7 +31,8 @@ with col1:
         locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
     )
     m.add_basemap(basemap)
-   m.to_streamlit(height=500)  # Set height and width
+    m.to_streamlit(height=700, width=1000)  # Set height and width
+
 
  
     # markdown = """
