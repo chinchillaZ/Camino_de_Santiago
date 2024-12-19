6a11eeb9c293036beeae45cd67c95f76e8529e14
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-20 02:16:08 +0800

    Update and rename 4_🚀_全球人次統計.py to 4_🎒_全球人次統計.py

---
+++ pages/4_🎒_全球人次統計.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+import pydeck as pdk
+
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://chinchillaz.github.io/streamlit-hw/logo_sun-removebg-preview.png"
+st.sidebar.image(logo)
+
+st.title("🎒_全球人次統計")
+
+
+left, middle, right = st.columns(3)
+if left.button("Plain button", use_container_width=True):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", use_container_width=True):
+    middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", use_container_width=True):
+    right.markdown("You clicked the Material button.")
+
+#m.to_streamlit(height=700)
