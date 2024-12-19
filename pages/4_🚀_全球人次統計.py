255ef39a86c01ffea5efb7212034c5aa10dec132
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-20 01:40:56 +0800

    Update 4_🚀_全球人次統計.py

--- pages/4_🚀_全球人次統計.py
+++ pages/4_🚀_全球人次統計.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import pydeck as pdk
 
 st.set_page_config(layout="wide")
 
@@ -15,16 +16,4 @@ st.sidebar.image(logo)
 
 st.title("Heatmap")
 
-with st.expander("See source code"):
-    with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4)
-        m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="pop_max",
-            name="Heat map",
-            radius=20,
-        )
 m.to_streamlit(height=700)
