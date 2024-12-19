dfce368f80f5fb75d5ea798f323c725af1c2ef43
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-19 12:32:11 +0800

    Update 1_🏔️_路線介紹.py

--- pages/1_🏔️_路線介紹.py
+++ pages/1_🏔️_路線介紹.py
@@ -27,7 +27,7 @@ with col2:
 
 
 with col1:
-    m = leafmap.Map(center = [42.5, -4.0], zoom = 7)
+    m = leafmap.Map(center = [42.5, -4.0], zoom = 6)
     m.add_basemap(basemap)
 
     def style_by_route(feature):
@@ -59,18 +59,18 @@ with col1:
 
 
  
-    # markdown = """
+    markdown = """
         
-    #     | Name                          | Distance (km) | Days | Challenge      |
-    # |-------------------------------|---------------|------|----------------|
-    # | 法國之路 (Camino Francés)       | 771           | 36   | Moderate       |
-    # | 葡萄牙之路 (Camino Portugués)   | 620           | 29   | Easy           |
-    # | 北方之路 (Camino del Norte)     | 481           | 23   | Easy           |
-    # | 原始之路 (Camino Primitivo)     | 16            | 16   | Moderate Plus  |
-    # | 銀之路 (Via de la Plata)        | 49            | 49   | Moderate Plus  |
-    # | 英國之路 (Camino Inglés)        | 114           | 7    | Moderate       |
-    # | 世界盡頭之路 (Camino Finisterre-Muxía) | 86            | 6    | Moderate       |
+        | Name                          | Distance (km) | Days | Challenge      |
+    |-------------------------------|---------------|------|----------------|
+    | 法國之路 (Camino Francés)       | 771           | 36   | Moderate       |
+    | 葡萄牙之路 (Camino Portugués)   | 620           | 29   | Easy           |
+    | 北方之路 (Camino del Norte)     | 481           | 23   | Easy           |
+    | 原始之路 (Camino Primitivo)     | 16            | 16   | Moderate Plus  |
+    | 銀之路 (Via de la Plata)        | 49            | 49   | Moderate Plus  |
+    | 英國之路 (Camino Inglés)        | 114           | 7    | Moderate       |
+    | 世界盡頭之路 (Camino Finisterre-Muxía) | 86            | 6    | Moderate       |
     
-    #     """
+        """
         
-    # st.markdown(markdown)
+    st.markdown(markdown)
