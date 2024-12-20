782860d29c5383f61a5ec917deb838eb814b164c
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-20 21:21:47 +0800

    Update and rename 8_☎️聯絡我們.py to 8_☎️聯絡資訊.py

---
+++ pages/8_☎️聯絡資訊.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+
+logo = "https://chinchillaz.github.io/streamlit-hw/logo_sun-removebg-preview.png"
+st.sidebar.image(logo)
+
+st.title("☎️ 聯絡資訊")
