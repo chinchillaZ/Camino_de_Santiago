d386b0c4a0a2e3171ae318742c46f99521458850
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-20 21:59:17 +0800

    Update 3_🏰_旅遊景點推薦.py

--- pages/3_🏰_旅遊景點推薦.py
+++ pages/3_🏰_旅遊景點推薦.py
@@ -5,13 +5,9 @@ import numpy as np
 
 st.set_page_config(layout="wide")
 
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
 
 st.sidebar.title("About")
-st.sidebar.info(markdown)
+
 logo = "https://chinchillaz.github.io/streamlit-hw/logo_sun-removebg-preview.png"
 st.sidebar.image(logo)
 
