3fd5638a9c76badb16a7b76cf2235e5d0bf3c4b3
chinchillaZ <145755529+chinchillaZ@users.noreply.github.com>
2024-12-20 13:20:59 +0800

    Update 7_🍽️_美食指南手冊.py

--- pages/7_🍽️_美食指南手冊.py
+++ pages/7_🍽️_美食指南手冊.py
@@ -15,38 +15,70 @@ st.markdown(
     """
 )
 st.title("西班牙")
-st.header("1.炭烤香料馬鈴薯章魚腳 Pulpo a la Gallega")
+
+# st.header("1.炭烤香料馬鈴薯章魚腳 Pulpo a la Gallega")
+# image_url = "https://github.com/liqi6/caminotest/blob/main/%E7%BE%8E%E9%A3%9F%E7%85%A7%E7%89%87/%E7%82%AD%E7%83%A4%E9%A6%99%E6%96%99%E9%A6%AC%E9%88%B4%E8%96%AF%E7%AB%A0%E9%AD%9A%E8%85%B3.jpeg?raw=true"
+# st.image(image_url, caption="Pulpo a la Gallega", use_container_width=True)
+
+# markdown = """
+# 源自西班牙西北部的加利西亞（Galicia）地區，加利西亞有著悠久的海鮮捕撈歷史，尤其是章魚，這使得章魚成為當地餐桌上的重要食材之一。而這道菜也是一種文化象徵，代表著加利西亞人對海洋資源和傳統烹飪的敬意。例如，在加利西亞的章魚節（Festa do Pulpo），這道菜會被大規模製作和享用，成為當地居民和遊客共同慶祝的中心。
+
+# 這道菜的主要特色是將章魚腳和馬鈴薯結合，經過燒烤和香料調味，口感鮮嫩且極具層次感，通常作為前菜或共享菜品來享用，搭配著一杯清爽的白葡萄酒。
+# """
+# st.markdown(markdown)
+
+# st.header("2.西班牙海鮮大鍋飯 Paella de Marisco")
+# image_url = "https://github.com/liqi6/caminotest/blob/main/%E7%BE%8E%E9%A3%9F%E7%85%A7%E7%89%87/%E8%A5%BF%E7%8F%AD%E7%89%99%E6%B5%B7%E9%AE%AE%E5%A4%A7%E9%8D%8B%E9%A3%AF.jpeg?raw=true"
+# st.image(image_url, caption="Paella de Marisco", use_container_width=True)
+
+# markdown = """
+# Paella（帕埃利亞）這個名字本身，來自於西班牙的瓦倫西亞地區（Valencia）。隨著西班牙飲食文化的傳播，Paella 也逐漸成為全球著名的西班牙美食之一。各地的餐廳和家庭都會根據自己的需求，創造出不同版本的 Paella。
+
+# 香料中的藏紅花不僅賦予飯菜金黃的顏色，也帶來特有的香氣，使整道菜味道更加豐富。海鮮的口感鮮嫩多汁，並且搭配著香脆的米飯底部（稱為socarrat），深受許多食客所喜愛。
+# """
+# st.markdown(markdown)
+
+# st.header("3.秘密西班牙松阪豬 Secreto de Cerdo a la Plancha")
+# image_url = "https://github.com/liqi6/caminotest/blob/main/%E7%BE%8E%E9%A3%9F%E7%85%A7%E7%89%87/%E7%A7%98%E5%AF%86%E8%A5%BF%E7%8F%AD%E7%89%99%E6%9D%BE%E9%98%AA%E8%B1%AC.jpeg?raw=true"
+# st.image(image_url, caption="Secreto de Cerdo a la Plancha", use_container_width=True)
+
+# markdown = """
+# 名稱中的“Secreto”意指豬肉的「秘密部位」，這是豬肉上非常嫩且風味極佳的一部分，來自豬肩膀附近的部位，因其形狀不規則且含有豐富的脂肪，通常在傳統的分肉過程中會被忽略不容易在傳統的肉類分切中發現，這也是它被稱為「秘密」的原因。
+
+# 松阪豬肉中含有較多的脂肪，這使得這道菜既有豐富的肉香，又不會過於油膩。煎烤後的豬肉表面帶有一層脆皮，內部依然保持嫩滑多汁，口感層次分明。橄欖油和鹽調味後，肉的鮮美被充分展現，簡單卻極為美味。
+# """
+# st.markdown(markdown)
+
+# First Dish: Pulpo a la Gallega
+st.subheader("1. 炭烤香料馬鈴薯章魚腳 Pulpo a la Gallega")
 image_url = "https://github.com/liqi6/caminotest/blob/main/%E7%BE%8E%E9%A3%9F%E7%85%A7%E7%89%87/%E7%82%AD%E7%83%A4%E9%A6%99%E6%96%99%E9%A6%AC%E9%88%B4%E8%96%AF%E7%AB%A0%E9%AD%9A%E8%85%B3.jpeg?raw=true"
 st.image(image_url, caption="Pulpo a la Gallega", use_container_width=True)
 
 markdown = """
-源自西班牙西北部的加利西亞（Galicia）地區，加利西亞有著悠久的海鮮捕撈歷史，尤其是章魚，這使得章魚成為當地餐桌上的重要食材之一。而這道菜也是一種文化象徵，代表著加利西亞人對海洋資源和傳統烹飪的敬意。例如，在加利西亞的章魚節（Festa do Pulpo），這道菜會被大規模製作和享用，成為當地居民和遊客共同慶祝的中心。
-
-這道菜的主要特色是將章魚腳和馬鈴薯結合，經過燒烤和香料調味，口感鮮嫩且極具層次感，通常作為前菜或共享菜品來享用，搭配著一杯清爽的白葡萄酒。
+源自西班牙西北部的加利西亞（Galicia）地區，加利西亞有著悠久的海鮮捕撈歷史，尤其是章魚，這使得章魚成為當地餐桌上的重要食材之一。這道菜的主要特色是將章魚腳和馬鈴薯結合，經過燒烤和香料調味，口感鮮嫩且極具層次感，通常作為前菜或共享菜品來享用，搭配著一杯清爽的白葡萄酒。
 """
 st.markdown(markdown)
 
-st.header("2.西班牙海鮮大鍋飯 Paella de Marisco")
+# Second Dish: Paella de Marisco
+st.subheader("2. 西班牙海鮮大鍋飯 Paella de Marisco")
 image_url = "https://github.com/liqi6/caminotest/blob/main/%E7%BE%8E%E9%A3%9F%E7%85%A7%E7%89%87/%E8%A5%BF%E7%8F%AD%E7%89%99%E6%B5%B7%E9%AE%AE%E5%A4%A7%E9%8D%8B%E9%A3%AF.jpeg?raw=true"
 st.image(image_url, caption="Paella de Marisco", use_container_width=True)
 
 markdown = """
-Paella（帕埃利亞）這個名字本身，來自於西班牙的瓦倫西亞地區（Valencia）。隨著西班牙飲食文化的傳播，Paella 也逐漸成為全球著名的西班牙美食之一。各地的餐廳和家庭都會根據自己的需求，創造出不同版本的 Paella。
-
-香料中的藏紅花不僅賦予飯菜金黃的顏色，也帶來特有的香氣，使整道菜味道更加豐富。海鮮的口感鮮嫩多汁，並且搭配著香脆的米飯底部（稱為socarrat），深受許多食客所喜愛。
+Paella（帕埃利亞）這個名字來自西班牙瓦倫西亞（Valencia）。香料中的藏紅花賦予飯菜金黃顏色和特有香氣，海鮮口感鮮嫩多汁，搭配香脆的米飯底部（socarrat），深受食客喜愛。
 """
 st.markdown(markdown)
 
-st.header("3.秘密西班牙松阪豬 Secreto de Cerdo a la Plancha")
+# Third Dish: Secreto de Cerdo a la Plancha
+st.subheader("3. 秘密西班牙松阪豬 Secreto de Cerdo a la Plancha")
 image_url = "https://github.com/liqi6/caminotest/blob/main/%E7%BE%8E%E9%A3%9F%E7%85%A7%E7%89%87/%E7%A7%98%E5%AF%86%E8%A5%BF%E7%8F%AD%E7%89%99%E6%9D%BE%E9%98%AA%E8%B1%AC.jpeg?raw=true"
 st.image(image_url, caption="Secreto de Cerdo a la Plancha", use_container_width=True)
 
 markdown = """
-名稱中的“Secreto”意指豬肉的「秘密部位」，這是豬肉上非常嫩且風味極佳的一部分，來自豬肩膀附近的部位，因其形狀不規則且含有豐富的脂肪，通常在傳統的分肉過程中會被忽略不容易在傳統的肉類分切中發現，這也是它被稱為「秘密」的原因。
-
-松阪豬肉中含有較多的脂肪，這使得這道菜既有豐富的肉香，又不會過於油膩。煎烤後的豬肉表面帶有一層脆皮，內部依然保持嫩滑多汁，口感層次分明。橄欖油和鹽調味後，肉的鮮美被充分展現，簡單卻極為美味。
+Secreto de Cerdo是一道來自西班牙的豬肉菜肴，名稱中的“Secreto”意指豬肉的「秘密部位」，豬肩部的嫩肉，煎烤後外脆內嫩。搭配橄欖油和鹽調味，肉香十足且不油膩，是一道簡單卻極美味的料理。
 """
 st.markdown(markdown)
+
 
 st.header("4.西班牙馬鈴薯烘蛋 Tortilla Espanola")
 image_url = "https://github.com/liqi6/caminotest/blob/main/%E7%BE%8E%E9%A3%9F%E7%85%A7%E7%89%87/%E8%A5%BF%E7%8F%AD%E7%89%99%E9%A6%AC%E9%88%B4%E8%96%AF%E7%83%98%E8%9B%8B.jpeg?raw=true"
